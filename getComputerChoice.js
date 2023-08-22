75f7db48dd649ec96e4de45597fc6028377fdab5
Justin Chau <chausicle@gmail.com>
2023-08-21 19:13:44 -0700

    Make computer choose rock, paper, or scissor

--- getComputerChoice.js
+++ getComputerChoice.js
@@ -1,1 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {});
+function getComputerChoice() {
+  const randomNum = Math.floor(Math.random() * 3) + 1
+  const choice = randomNum === 1 
+    ? "rock" : randomNum === 2 
+    ? "paper" : "scissor"
+  
+  return choice
+}
