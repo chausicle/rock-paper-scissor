a5660205c9f2b3750eb2447e8358f26de7553e16
Justin Chau <chausicle@gmail.com>
2023-09-19 01:22:25 -0700

    Keep track of wins between player and computer

--- game.js
+++ game.js
@@ -1,17 +1,37 @@
 document.addEventListener("DOMContentLoaded", game);
 
+function decideFate(playerCount, computerCount) {
+  if (playerCount === 5) {
+    const rpsButtons = document.querySelectorAll("button");
+    rpsButtons.forEach(choice => {
+      choice.removeEventListener("click", playerSelection)
+    });
+  } else if (computerCount === 5) {
+    const rpsButtons = document.querySelectorAll("button");
+    rpsButtons.forEach(choice => {
+      choice.removeEventListener("click", playerSelection)
+    });
+  }
+}
+
 function playerSelection(e) {
   const playerChoice = e.target.getAttribute("id");
   const computerChoice = getComputerChoice();
-  const playerResult = document.querySelector("div#playerResult")
-  const computerResult = document.querySelector("div#computerResult")
+  const playerCount = document.querySelector("#playerCount");
+  const computerCount = document.querySelector("#computerCount");
+  const playerResult = document.querySelector("div#playerResult");
+  const computerResult = document.querySelector("div#computerResult");
   const description = document.querySelector("p#description");
-  const [player, computer, result] = playRound(playerChoice, computerChoice);
+  const [player, computer, result, playerIncrement, computerIncrement] = playRound(playerChoice, computerChoice);
 
   console.log("PlayerChoice: ", playerChoice, " - ComputerChoice: ", computerChoice);
+  playerCount.textContent = Number(playerCount.textContent) + playerIncrement;
+  computerCount.textContent = Number(computerCount.textContent) + computerIncrement;
   playerResult.textContent = player;
   computerResult.textContent = computer;
   description.textContent = result;
+  
+  decideFate(Number(playerCount.textContent), Number(computerCount.textContent))
 }
 
 function game() {
--- playRound.js
+++ playRound.js
@@ -2,27 +2,27 @@ function playRound(playerSelection, computerSelection) {
   switch (playerSelection) {
     case "rock":
       if (computerSelection === "rock") {
-        return ["DRAW", "DRAW", "DRAW"];
+        return ["DRAW", "DRAW", "DRAW", 0, 0];
       } else if (computerSelection === "paper") {
-        return ["FAILURE", "SUCCESSFUL", "YOU LOSE... Rock is quickly overwhelmed by Paper's suffocating grasp"];
+        return ["FAILURE", "SUCCESSFUL", "YOU LOSE... Rock is quickly overwhelmed by Paper's suffocating grasp", 0, 1];
       } else {
-        return ["SUCCESSFUL", "FAILURE", "YOU WIN! Rock smashes Scissor leaving it extremely blunt and lost in search of its purpose in life"];
+        return ["SUCCESSFUL", "FAILURE", "YOU WIN! Rock smashes Scissor leaving it extremely blunt and lost in search of its purpose in life", 1, 0];
       }
     case "paper":
       if (computerSelection === "rock") {
-        return ["SUCCESSFUL", "FAILURE", "YOU WIN! Paper puts Rock in a choke hold till it passes out stone cold"];
+        return ["SUCCESSFUL", "FAILURE", "YOU WIN! Paper puts Rock in a choke hold till it passes out stone cold", 1, 0];
       } else if (computerSelection === "paper") {
-        return ["DRAW", "DRAW", "DRAW"];
+        return ["DRAW", "DRAW", "DRAW", 0, 0];
       } else {
-        return ["FAILURE", "SUCCESSFUL", "YOU LOSE... As fast as Paper saw the light, it split into two by Scissor's deadly quick draw of the blade"];
+        return ["FAILURE", "SUCCESSFUL", "YOU LOSE... As fast as Paper saw the light, it split into two by Scissor's deadly quick draw of the blade", 0, 1];
       }
     case "scissor":
       if (computerSelection === "rock") {
-        return ["FAILURE", "SUCCESSFUL", "YOU LOSE... Scissor sends a flurry of slashes but quickly dulls out from Rock's impenetrable defense"];
+        return ["FAILURE", "SUCCESSFUL", "YOU LOSE... Scissor sends a flurry of slashes but quickly dulls out from Rock's impenetrable defense", 0, 1];
       } else if (computerSelection === "paper") {
-        return ["SUCCESSFUL", "FAILURE", `YOU WIN! Before Paper could fold in defeat, it splits into two clean halves`];
+        return ["SUCCESSFUL", "FAILURE", `YOU WIN! Before Paper could fold in defeat, it splits into two clean halves`, 1, 0];
       } else {
-        return ["DRAW", "DRAW", "DRAW"];
+        return ["DRAW", "DRAW", "DRAW", 0, 0];
       }
     default:
       return "Hmm, I don't suppose you know how to play the game... That's alright, try again!";
